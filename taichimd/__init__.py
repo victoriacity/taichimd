7aeec6da7281e4d91289f6ee4e7100c7b3c0469b
Andrew Sun <victoriacity74@gmail.com>
2020-07-10 22:46:39 -0500

    v0.0.1

---
+++ taichimd/__init__.py
@@ -0,0 +1,5 @@
+
+from .consts import *
+from .system import MolecularDynamics
+
+__version__ = "0.0.1"
---
+++ taichimd/ui.py
@@ -0,0 +1,74 @@
+import taichi as ti
+import numpy as np
+
+WINDOW_SIZE = 1024
+rgb2hex = lambda x: x[0].astype(np.int) * 65536 + x[1].astype(np.int) * 256 + x[2].astype(np.int)
+
+
+class GUI:
+
+    def __init__(self, system, shader):
+        self.system = system
+        self.gui = ti.GUI("MD", res=WINDOW_SIZE)
+        self.shader = shader(self.gui)
+
+    def show_temperature(self):
+        t_actual = self.system.get_temp()
+        t_set = self.system.temperature
+        while self.gui.get_event(ti.GUI.PRESS):
+            if self.gui.event.key == ti.GUI.ESCAPE:
+                exit()
+            if self.gui.event.key == ti.GUI.UP:
+                t_set = round(t_set + 0.1, 1)
+                self.system.set_temp(t_set)
+            if self.gui.event.key == ti.GUI.DOWN:
+                if t_set <= 0.15:
+                    t_set /= 2
+                else:
+                    t_set = round(t_set - 0.1, 1)
+                self.system.set_temp(t_set) 
+        color_t = 0xf56060 if abs(t_actual - t_set) > 0.02 else 0x74e662
+        self.gui.text("T_set = %.3g" % t_set, (0.05, 0.2), font_size=36)
+        self.gui.text("T_actual = %.3g" % t_actual, (0.05, 0.15), font_size=36, color=color_t)
+
+
+    def show(self, savefile=None, draw_bonds=False):
+        nbonds = self.system.nbonds if draw_bonds else 0
+        self.shader.render(self.system.position.to_numpy() \
+            / self.system.boxlength,
+            nbonds, self.system.bond_table)
+        if self.system.temperature > 0:
+            self.show_temperature()
+        self.gui.text("Internal energy = %.3f" % (self.system.energy()), (0.05, 0.1), font_size=36)
+        self.gui.show(savefile)
+        
+
+
+class Shader:
+
+    def __init__(self, gui):
+        self.gui = gui
+
+    def render(self, positions):
+        raise NotImplementedError
+
+class CanvasShader(Shader):
+    camera = 1
+    radius = 8
+    bg = np.array([17, 47, 65])
+    circ = np.array([122, 200, 225])
+    bond = np.array([62, 165, 45])
+
+    def render(self, positions, nbonds=0, bond_table=None):
+        self.gui.clear(rgb2hex(self.bg))   
+        z = positions[:, 2]
+        xy = positions[:, :2]
+        z_order = np.argsort(z)
+        sizes = self.radius * self.camera / (self.camera + (1 - z))
+        z /= np.max(z)
+        colors = rgb2hex(np.outer(self.bg, 1 - z) + np.outer(self.circ, z))
+        for n in range(nbonds):
+            i = bond_table[n][0]
+            j = bond_table[n][1]
+            self.gui.line(xy[i], xy[j], color=rgb2hex(self.bond), radius=self.radius / 3)
+        self.gui.circles(xy, radius=sizes[z_order], color=colors[z_order])
